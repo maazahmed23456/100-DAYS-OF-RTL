ae73c7a1c68e1f0b276088d044b4a25c0c0fbfd0
maazahmed23456 <116651816+maazahmed23456@users.noreply.github.com>
2024-03-09 12:16:51 +0530

    Add files via upload

---
+++ DAY 35 - SHIFT ROTATE UNIT/SHIFT_ROTATE_UNIT_TB.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.03.2024 11:58:18
+// Design Name: 
+// Module Name: SHIFT_ROTATE_UNIT_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module shift_rotate_tb;
+
+	// Inputs
+	reg [7:0] a;
+	reg [2:0] opcode;
+
+	// Outputs
+	wire [7:0] result;
+	integer i;
+
+	// Instantiate the Unit Under Test (UUT)
+	shift_rotate uut (
+		.a(a), 
+		.opcode(opcode), 
+		.result(result)
+	);
+
+	initial begin
+	a=8'b10110101;
+	opcode = 000;
+		
+	end
+	
+	initial
+	begin
+	for(i=0;i<6;i=i+1)
+	begin
+	opcode=i;
+	#10;
+	end
+	end
+	
+	initial
+	$monitor(" A=%b | Opcode=%b | Result =%b ",a,opcode,result);
+      
+endmodule
